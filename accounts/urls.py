7e9cb6879d105a2a2414b543027f0eea94acbc4e
leesk3732 <leesk3732@naver.com>
2021-11-18 14:16:41 +0900

    회원가입

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+from rest_framework_jwt.views import obtain_jwt_token
 app_name = 'accounts'
 urlpatterns = [
-    
+    path('signup/', views.signup),
+    path('api-token-auth/',obtain_jwt_token),
+    # path('login/', views.login, name='login'),
+    # path('logout/', views.logout, name='logout'),
 ]
--- finalpjt/settings.py
+++ finalpjt/settings.py
@@ -136,3 +136,19 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+import datetime
+JWT_AUTH={
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
+}
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+
+    ),
+}
